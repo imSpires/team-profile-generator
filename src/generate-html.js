0d615eaae90739b484170699c13c8eb5afbc36f8
Ryan Dungan <rd3@knights.ucf.edu>
2021-08-29 18:56:50 -0400

    HTML is generated to output folder

--- src/generate-html.js
+++ src/generate-html.js
@@ -4,16 +4,74 @@ const createTeamProfile = (team) => {
     const html = [];
 
     const createManagerHTML = manager => {
-
+        console.log(manager);
+        let managerHTML = `
+        <div class="role-container">
+            <div class="role-header">
+            <h1>Manager</h1>
+            <h2>Name: ${manager.name}</h2>
+            </div>
+            <div class="role-information-container>
+                <h3>ID: ${manager.id}</h3>
+                <h3>E-Mail: <a href="mailto:${manager.email}">${manager.email}</a></h3>
+                <h3>Office #: ${manager.officeNo}</h3>
+            </div>
+        </div>
+        `;
+        html.push(managerHTML);
     }
 
     const createEngineerHTML = engineer => {
-
+        console.log(engineer);
+        let engineerHTML = `
+        <div class="role-container">
+            <div class="role-header">
+            <h1>Engineer</h1>
+            <h2>Name: ${engineer.name}</h2>
+            </div>
+            <div class="role-information-container>
+                <h3>ID: ${engineer.id}</h3>
+                <h3>E-Mail: <a href="mailto:${engineer.email}">${engineer.email}</a></h3>
+                <h3>Github: <a target="_blank" href="https://github.com/${engineer.github}">${engineer.github}</a></h3>
+            </div>
+        </div>
+        `;
+        html.push(engineerHTML);
     }
 
     const createInternHTML = intern => {
-        
+        console.log(intern);
+        let internHTML = `
+        <div class="role-container">
+            <div class="role-header">
+            <h1>Intern</h1>
+            <h2>Name: ${intern.name}</h2>
+            </div>
+            <div class="role-information-container>
+                <h3>ID: ${intern.id}</h3>
+                <h3>E-Mail: <a href="mailto:${intern.email}">${intern.email}</a></h3>
+                <h3>School: ${intern.school}</h3>
+            </div>
+        </div>
+        `;
+        html.push(internHTML);
     }
+
+    // loop through roles to generate HTML
+    for (let i = 0; i < team.length; i++ ) {
+        if (team[i].getRole() === "Manager") {
+            createManagerHTML(team[i]);
+        }
+        if (team[i].getRole() === "Engineer") {
+            createEngineerHTML(team[i]);
+        }
+        if (team[i].getRole() === "Intern") {
+            createInternHTML(team[i]);
+        }
+    }
+
+    // Combine everything and return to be exported
+    return html.join('');
 }
 
 module.exports = team => {
@@ -26,6 +84,9 @@ module.exports = team => {
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="../assets/style.css">
         <title>Team Profile</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative&display=swap" rel="stylesheet">
     </head>
 
     <body>
@@ -35,7 +96,6 @@ module.exports = team => {
         
         <main>${createTeamProfile(team)}</main>
 
-        <script src="js/scripts.js"></script>
     </body>
 
     </html>`
